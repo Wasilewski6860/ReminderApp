e43912cdc08d1963f5fe67479441e97f475a0c2d
ProgramNotWorking <Ve7ter@yandex.ru>
2024-04-24 19:58:04 +0300

    test commit

--- app/src/main/java/com/example/reminderapp/MainActivity.kt
+++ app/src/main/java/com/example/reminderapp/MainActivity.kt
@@ -12,5 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.apply {
+
+            
+
+        }
     }
 }
