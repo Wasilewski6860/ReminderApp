cdc5de844688d720d7be46ff74df53365ddf8671
ProgramNotWorking <Ve7ter@yandex.ru>
2024-06-11 16:44:48 +0300

    Global refactoring

--- app/src/main/java/com/example/reminderapp/presentation/mainscreen/MainFragment.kt
+++ app/src/main/java/com/example/reminderapp/presentation/mainscreen/MainFragment.kt
@@ -19,49 +19,12 @@ class MainFragment : Fragment() {
     private lateinit var adapter: MainScreenRecyclerViewAdapter
     private val viewModel by viewModel<MainViewModel>()
 
-    init {
-        // LiveData observing here (for now one i guess)
-    }
-
-    companion object {
-        const val TASK_DATA = "data"
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = MainFragmentBinding.inflate(inflater, container, false)
-
-        binding.apply {
-
-            adapter = MainScreenRecyclerViewAdapter(object : MainScreenRecyclerViewAdapter.OnItemClickListener {
-                override fun onRcItemClick(position: Int) {
-                    val data = Gson().toJson(adapter.getItemByPosition(position))
-                    val bundle = Bundle().apply {
-                        putString(TASK_DATA, data)
-                    }
-                    val action = R.id.action_mainFragment_to_creatorFragment
-                    findNavController().navigate(
-                        resId = action,
-                        args = bundle,
-                        navOptions = NavOptions.Builder()
-                            .setEnterAnim(R.anim.slide_in_anim)
-                            .build()
-                    )
-                }
-            }, requireActivity().applicationContext)
-
-            mainScreenRecyclerView.layoutManager = GridLayoutManager(context, 1)
-            mainScreenRecyclerView.adapter = adapter
-            
-            viewModel.fetchTasksFromDatabase()
-            viewModel.tasksListData.observe(viewLifecycleOwner) { itemsList ->
-                adapter.fillRecyclerWithFullItemsList(itemsList)
-            }
-
-        }
 
         return binding.root
     }
